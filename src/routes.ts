7504956ed9b06449997504e0a57d2265a7bf25e1
Hetari <hetari4py@gmail.com>
2024-10-13 16:04:53 +0300

    add: 404 page

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { bg1, bg2, bg3, bg4 } from '@/assets/images';
 const bgImages = [bg1, bg2, bg3, bg4];
-import { createWebHistory, createRouter } from 'vue-router';
+import { createWebHistory, createRouter, RouteMeta } from 'vue-router';
 import Home from './views/Home.vue';
 import Register from './views/Register.vue';
 import Login from './views/Login.vue';
+
+interface IRouteMeta {
+  title: string;
+}
 
 export const base = '/health-care';
 const routes = [
@@ -11,18 +15,36 @@ const routes = [
     path: `${base}/`,
     name: 'Home',
     component: Home,
+    meta: {
+      title: 'Home',
+    } as RouteMeta & IRouteMeta,
   },
   {
     path: `${base}/register`,
     name: 'Register',
     component: Register,
     beforeEnter: [preloadImage],
+    meta: {
+      title: 'Register',
+    } as RouteMeta & IRouteMeta,
   },
   {
     path: `${base}/login`,
     name: 'Login',
     component: Login,
     beforeEnter: [preloadImage],
+    meta: {
+      title: 'Login',
+    } as RouteMeta & IRouteMeta,
+  },
+  // 404 page
+  {
+    path: '/:pathMatch(.*)',
+    name: 'not-found',
+    component: () => import('@/views/404.vue'),
+    meta: {
+      title: 'Page Not Found',
+    } as RouteMeta & IRouteMeta,
   },
 ];
 
@@ -32,6 +54,9 @@ const router = createRouter({
 });
 
 function preloadImage(to, from, next) {
+  // if (window.innerWidth < 1024) {
+  //   return next();
+  // }
   const randomImage = bgImages[Math.floor(Math.random() * bgImages.length)];
   const img = new Image();
   img.src = randomImage;
---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex h-screen w-screen items-center justify-center">
+    <div class="flex w-1/2 flex-col items-center">
+      <h2 class="text-center text-8xl font-bold">404</h2>
+      <h3 class="my-2 text-center text-2xl font-medium">Not Found</h3>
+      <p class="text-foreground/30 text-center text-sm">
+        The page you're trying to access <br />
+        could not be found
+      </p>
+      <RouterLink to="/dashboard/home">
+        <Button class="mt-4" variant="outline" prepend-icon="ArrowLeft"
+          >Back to Home</Button
+        >
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { Button } from '@/components/ui/button';
+</script>
